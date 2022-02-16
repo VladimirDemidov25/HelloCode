ef60694cbfb408d029ae60aa48d4cf5f8396fa6d
«VladimirDemidov25» <8387348@gmail.com>
2022-02-16 23:07:19 +0300

    MaxNum

---
+++ Examples/Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 10;
+int b = 287;
+int c = 6;
+int d = 865;
+int e = 422;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
