b03bec27f0af153e71afccea569ef0e5c24ba01c
«VladimirDemidov25» <8387348@gmail.com>
2022-02-28 21:14:22 +0300

    является ли одно число квадратом второго

---
+++ Examples1/Examples004/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+string ss = ReadLine();
+{
+    Write("Добавь первое число");
+}
+int a = int.Parse(ss);
+
+string ee = ReadLine();
+{
+    Write("Добавь второе число");
+}
+int b = int.Parse(ee);
+
+if (a/a == b/b)
+{
+    WriteLine("Все круто, бро");
+    return;
+}
+    WriteLine("Все херово, бро");
