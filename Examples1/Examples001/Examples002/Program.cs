6549758c74d641516fa5dabb88bb03f99cfbc207
«VladimirDemidov25» <8387348@gmail.com>
2022-02-28 19:24:16 +0300

    GameOver

--- Examples1/Examples001/Examples002/Program.cs
+++ Examples1/Examples001/Examples002/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ann, I love You");
+﻿int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.Write("Понедельник");
+}
+
+if (a == 2)
+{
+    Console.Write("Вторник");
+}
+
+if (a == 3)
+{
+    Console.Write("Среда");
+}
+
+if (a == 4)
+{
+    Console.Write("Четверг");
+}
+
+if (a == 5)
+{
+    Console.Write("Пятница");
+}
+
+if (a == 6)
+{
+    Console.Write("Суббота");
+}
+
+if (a == 7)
+{
+    Console.Write("Восскресенье");
+}
+
+if (a > 7)
+{
+    Console.Write("Ann, ILoveYou");
+}
